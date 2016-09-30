1a2735ea64701a739a625e94ec7d03ddc7d33288
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-09-30 02:01:21 +0200

    started re-writing  original wrapper

--- src/netReg.cpp
+++ src/netReg.cpp
@@ -3,23 +3,20 @@
  * Email: netreg@simon-dirmeier.net
  */
 
+#include "graph_penalized_linear_model_data.hpp"
+#include <string>
+
+#include "graph_penalized_linear_model_data.hpp"
+#include "edgenet.hpp"
+#include "gaussian_edgenet.hpp"
+#include "binomial_edgenet.hpp"
+
 #include <R.h>
 #include <Rinternals.h>
 #include <Rmath.h>
-
-#include "edgenet_wrapper.hpp"
-#include "graph_penalized_linear_model_data.hpp"
-
 /**
  * Extern C++ binding for R! (name mangling)
  */
-
-double *B_;
-double *mu_;
-int *foldid_;
-double lamb_;
-double psi_gx_;
-double psi_gy_;
 
 extern "C"
 {
@@ -75,10 +72,27 @@ SEXP edgenet_(SEXP XS, SEXP YS,
     // get family
     const char *family = CHAR(STRING_ELT(familys, 0));
     // call wrapper
-    do_edgenet_(X, Y, GX, GY,
-                N, P, Q,
-                lambda, psigx, psigy,
-                niter, thresh, family);
+    netreg::graph_penalized_linear_model_data data(X, Y,
+                                                   GX, GY,
+                                                   N, P, Q,
+                                                   lambda, 1.0,
+                                                   psigx, psigy,
+                                                   n_iter, thresh);
+    netreg::edgenet* edge;
+    if (1 == 2)
+    {
+        edge = new netreg::binomial_edgenet;
+    }
+    else if (2 == 2)
+    {
+        edge = new netreg::gaussian_edgenet;
+    }
+    else
+    {
+        return R_Nil_Value;
+    }
+    edge->run(data);
+
     // protection counter that is needed for not activating garbage collection
     int prtCnt = 0;
     // R object for coefficient matrix
@@ -94,11 +108,11 @@ SEXP edgenet_(SEXP XS, SEXP YS,
     for (int i = 0; i < Q; ++i)
     {
         // safe intercepts for R vector
-        b0[i] = mu_[i];
+        //b0[i] = mu_[i];
         for (int j = 0; j < P; ++j)
         {
             // safe coefficients for R matrix
-            B[j + P * i] = B_[j + P * i];
+            //B[j + P * i] = B_[j + P * i];
         }
     }
     // create a R list of size 2 that can be returned
@@ -121,95 +135,5 @@ SEXP edgenet_(SEXP XS, SEXP YS,
     return OS;
 }
 
-/**
- * Implementation of cross-validation for Edgenet. Finds and returns the optimal shrinkage values given a specific data-set.
- *
- * @param XS the (ns x ps)-dimensional design matrix
- * @param YS the (ns x qs)-dimensional response matrix
- * @param GXS the (ps x ps)-prior graph for XS
- * @param GYS the (qs x qs)-prior graph for YS
- * @param ns number of observations/samples/rows
- * @param ps number of covariates
- * @param qs number of responses
- * @param psi_gxs weighting value of GX
- * @param psi_gys weighting value of GY
- * @param niters max number of iterations if parameter estimation does not converge in time
- * @param threshs convergence threshold
- * @param nfolds the number of cross-validation sets created (as in k-fold cv)
- * @param foldids integer vector of assignments of observations to folds (i.e. vector of ns elements,  \in {1, ..., nfolds}
- * @param lenfoldids length of the vector above
- */
-SEXP cv_edgenet_(SEXP XS, SEXP YS, SEXP GXS, SEXP GYS,
-                 SEXP ns, SEXP ps, SEXP qs,
-                 SEXP psi_gxs, SEXP psi_gys,
-                 SEXP niters, SEXP threshs,
-                 SEXP nfolds, SEXP foldids, SEXP lenfoldids,
-                 SEXP familys)
-{
-    double *X = REAL(XS);
-    // cast R response matrix to pointer to double
-    double *Y = REAL(YS);
-    // cast R prior matrix for X to pointer to double
-    double *GX = REAL(GXS);
-    // cast R prior matrix for Y to pointer to double
-    double *GY = REAL(GYS);
-    // get number of samples
-    const int N = (*INTEGER(ns));
-    // get number of covariables
-    const int P = (*INTEGER(ps));
-    // get number of responses
-    const int Q = (*INTEGER(qs));
-    // cast R design matrix to pointer to double
-    // cast R weighting values for GX to pointer to double
-    const double psigx = (*REAL(psi_gxs));
-    // cast R weighting values for GY to pointer to double
-    const double psigy = (*REAL(psi_gys));
-    // get number of max iterations
-    const int niter = (*INTEGER(niters));
-    // get convergence threshold
-    const double thresh = (*REAL(threshs));
-    // the number of folds
-    const int nfold = (*INTEGER(nfolds));
-    // the fold assignments (usually not given)
-    int *fold_ids = (INTEGER(foldids));
-    // the length of the fold_ids array
-    const int foldid_len = (*INTEGER(lenfoldids));
-    // get family
-    const char *family = CHAR(STRING_ELT(familys, 0));
-    // call wrapper
-    do_cv_edgenet_(X, Y, GX, GY,
-                   N, P, Q, psigx, psigy, niter, thresh,
-                   nfold, fold_ids, foldid_len, family);
-    // protection counter that is needed for not activating garbage collection
-    int prtCnt = 0;
-    SEXP shrink = PROTECT(allocVector(REALSXP, 3));
-    prtCnt++;
-    REAL(shrink)[0] = lamb_;
-    REAL(shrink)[1] = psi_gx_;
-    REAL(shrink)[2] = psi_gy_;
-    // fold ids
-    SEXP folds = PROTECT(allocVector(INTSXP, N));
-    prtCnt++;
-    for (int i = 0; i < N; ++i) INTEGER(folds)[i] = foldid_[i];
-    delete[] foldid_;
-    // create a R list of size 2 that can be returned
-    SEXP OS = PROTECT(allocVector(VECSXP, 2));
-    prtCnt++;
-    // set first element of list to the coef matrix
-    SET_VECTOR_ELT(OS, 0, shrink);
-    // set second element of list to intercept vector
-    SET_VECTOR_ELT(OS, 1, folds);
-    // create name array
-    SEXP nms = PROTECT(allocVector(STRSXP, 2));
-    prtCnt++;
-    SET_STRING_ELT(nms, 0, mkChar("shrinkage_parameters"));
-    SET_STRING_ELT(nms, 1, mkChar("fold_ids"));
-    // assign names to list
-    setAttrib(OS, R_NamesSymbol, nms);
-    // release SEXPs for garbage collection
-    UNPROTECT(prtCnt);
-    // return results to R
-    return OS;
-}
 
 };
