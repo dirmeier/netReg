5e845b8ff7379b66bfed676a4faead6dba147fcb
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-10-03 21:38:27 +0200

    R files licenses

--- R/netReg-package.R
+++ R/netReg-package.R
@@ -1,8 +1,30 @@
+# netReg: graph-regularized linear regression models.
+#
+# Copyright (C) 2015 - 2016 Simon Dirmeier
+#
+# This file is part of netReg.
+#
+# netReg is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# netReg is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with netReg. If not, see <http://www.gnu.org/licenses/>.
+
+
 #' netReg
 #'
-#' \emph{netReg} is a package for generalized linear regression that includes prior graphs in the models objective function.
-#' The models are a generalization of the \emph{LASSO} by Tibshirani.
-#' \emph{netReg} uses \emph{Armadillo}, \emph{Intel MKL}, \emph{OpenBLAS}, \emph{BLAS} and \emph{LAPACK} for fast matrix computations and
+#' \emph{netReg} is a package for generalized linear regression that includes 
+#' prior graphs in the models objective function. The models are a 
+#' generalization of the \emph{LASSO} by Tibshirani.
+#' \emph{netReg} uses \emph{Armadillo}, \emph{Intel MKL}, \emph{OpenBLAS}, 
+#' \emph{BLAS} and \emph{LAPACK} for fast matrix computations and
 #' \emph{Dlib} for constrained derivate-free optimization.
 #' 
 #' @name netReg-package
