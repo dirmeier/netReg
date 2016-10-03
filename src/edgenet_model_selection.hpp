8c0ff0ca415a5b174603b15355bfc70844f894cd
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-10-03 21:28:14 +0200

    c++ files licenses

--- src/edgenet_model_selection.hpp
+++ src/edgenet_model_selection.hpp
@@ -1,12 +1,30 @@
 /**
- * Author: Simon Dirmeier
- * Email: netreg@simon-dirmeier.net
+ * netReg: graph-regularized linear regression models.
+ * <p>
+ * Copyright (C) 2015 - 2016 Simon Dirmeier
+ * <p>
+ * This file is part of netReg.
+ * <p>
+ * netReg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * netReg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with netReg. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author: Simon Dirmeier
+ * @email: simon.dirmeier@gmx.de
  */
+#ifndef NETREG_EDGENET_MODEL_SELECTION_HPP
+#define NETREG_EDGENET_MODEL_SELECTION_HPP
 
-#ifndef NETREG_EDGENETMODELSELECTION_HPP
-#define NETREG_EDGENETMODELSELECTION_HPP
-
-#include    "graph_penalized_linear_model_cv_data.hpp"
+#include "graph_penalized_linear_model_cv_data.hpp"
 #include "cv_set.hpp"
 
 namespace netreg
--- src/netReg.cpp
+++ src/netReg.cpp
@@ -1,6 +1,25 @@
 /**
- * Author: Simon Dirmeier
- * Email: netreg@simon-dirmeier.net
+ * netReg: graph-regularized linear regression models.
+ * <p>
+ * Copyright (C) 2015 - 2016 Simon Dirmeier
+ * <p>
+ * This file is part of netReg.
+ * <p>
+ * netReg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * netReg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with netReg. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author: Simon Dirmeier
+ * @email: simon.dirmeier@gmx.de
  */
 
 #include <memory>
