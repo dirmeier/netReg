c42a1d486d1c088d379bceeb84b32e97b196e41c
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-02-19 20:02:48 +0100

    added some code to benchmarkings

--- benchmark/edgenet_benchmark_rss.R
+++ benchmark/edgenet_benchmark_rss.R
@@ -2,28 +2,43 @@ library(netReg)
 library(glmnet)
 library(uuid)
 
+cms    <- commandArgs(trailingOnly=T)
+stopifnot(length(cms) != 3)
+p      <- as.integer(cms[1])
+stopifnot(!is.na(p), is.numeric(p))
+q      <- as.integer(cms[2])
+stopifnot(!is.na(q), is.numeric(q))
+sig      <- as.integer(cms[3])
+stopifnot(!is.na(sig), is.numeric(sig))
 
-n      <- 100
-p      <- 10
-q      <- 1
+n      <- 1000
 thresh <- 1e-7
-maxit  <- 10000
+maxit  <- 100000
 
+G.X <- matrix(0, p, p) 
+G.X[1:floor(p/3), 1:floor(p/3)]                       <- 1
+G.X[floor(p/3):floor(2*p/3), floor(p/3):floor(2*p/3)] <- 1
+G.X[floor(2*p/3):p, floor(2*p/3):p]                   <- 1
+diag(G.X)                                             <- 0
 
-G.X <- matrix(1, p, p) 
-G.X[1:10, 1:10]   <- 5
-G.X[40:50, 40:50] <- 1
-G.X[80:90, 80:90] <- 1
-
+G.Y <- matrix(0, q, q) 
+G.Y[1:floor(q/3), 1:floor(q/3)]                       <- 1
+G.Y[floor(q/3):floor(2*q/3), floor(q/3):floor(2*q/3)] <- 1
+G.Y[floor(2*q/3):q, floor(2*q/3):q]                   <- 1
+diag(G.Y)                                             <- 0
 
 X <- matrix(rnorm(n*p), n, p)
 B <- matrix(rnorm(p*q), p, q)
-E <- matrix(rnorm(n*q), n, q)
+E <- matrix(rnorm(n*q, 0, sig), n, q)
 Y <- X %*% B + E
 
-G.Y <- matrix(1, q, q) 
-G.Y[upper.tri(G.Y)] <- rbeta(q * (q-1) / 2, 1, 2)
-G.Y <- t(G.Y) + G.Y
-
-cv.lasso <- cv.glmnet(X, Y, type.measure="mse") 
-cv.edge  <- cv.edgenet(X, Y, G.X, thresh=thresh, maxit=maxit, family="gaussian" )
+for (cv in 1:10)
+{
+  cve <- list()
+  for (i in 1:q)
+  {
+    cve[[i]] <- cv.glmnet(X, Y[,i], type.measure="mse", thresh=thresh, 
+                          standardize=F, maxit=maxit) 
+  }
+  cv.edge  <- cv.edgenet(X, Y, G.X=G.Y, G.Y=G.Y, thresh=thresh, maxit=maxit, family="gaussian" )
+}
