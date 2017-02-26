d6a8cdba1de32d46a24952d40381a8f35f33937b
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-02-26 20:23:01 +0100

    [benchmark] added cmdline flags to benchmark script

--- benchmark/edgenet_benchmark_time.R
+++ benchmark/edgenet_benchmark_time.R
@@ -1,22 +1,25 @@
-#source("./edgenet_pure.R")
+source("./edgenet_pure.R")
 library(netReg)
 library(microbenchmark)
 library(uuid)
 
-p <- 10
+args    <- commandArgs(trailingOnly=T)
+stopifnot(length(args) != 4)
+n <- p <- 0
+for (i in seq(args))
+{
+  if      (args[i] == "-n") n <- as.integer(cms[i+1])  
+  else if (args[i] == "-p") p <- as.integer(cms[i+1])  
+}
+cat(paste0("Measuring time with n=", n, ", p=", p))
+stop("2")
+stopifnot(!is.na(p), is.numeric(p), !is.na(n), is.numeric(n))
 
-#cms    <- commandArgs(trailingOnly=T)
-#stopifnot(length(cms) != 0)
-#p      <- as.integer(cms[1])
-#stopifnot(!is.na(p), is.numeric(p))
-#p <- 1000
-
-n      <- 1000
 q      <- 1
 lambda <- 10
 psigx  <- psigy <- 5
 thresh <- 1e-7
-maxit  <- 10000
+maxit  <- 100000
 
 X <- matrix(rnorm(n*p), n, p)
 B <- matrix(rnorm(p*q), p, q)
@@ -30,17 +33,18 @@ G.Y <- matrix(1, q, q)
 G.Y[upper.tri(G.Y)] <- rbeta(q * (q-1) / 2, 1, 2)
 G.Y <- t(G.Y) + G.Y
 
-microbenchmark(
-  #Pure=edgenet.pure.R(X, Y, G.X, G.Y, lambda, psigx, psigy, thresh, maxit),
-  Cpp=.Call("edgenet_cpp", X, Y, G.X, G.Y,
-            as.double(lambda), as.double(0),  as.double(0),
-            as.integer(maxit), as.double(thresh),
-            as.character("gaussian"))$coefficients,
-  times=10)
+m <- 
+  microbenchmark(
+    Pure=edgenet.pure.R(X, Y, G.X, G.Y, lambda, psigx, psigy, thresh, maxit),
+    Cpp=.Call("edgenet_cpp", X, Y, G.X, G.Y,
+              as.double(lambda), as.double(psigx),  as.double(psigy),
+              as.integer(maxit), as.double(thresh),
+              as.character("gaussian"))$coefficients,
+    times=10)
 
-#uuid <- uuid::UUIDgenerate()
-#path                         <- "~/PROJECTS/netreg_project/results/"
-#if (!file.exists(path)) path <- "/cluster/home/simondi/results/netReg/"
-#path <- paste0(path, "benchmark_", p, "_", uuid, ".rds")
-#cat(paste("going to:", path, "\n"))
-#saveRDS(m , path)
+uuid                         <- uuid::UUIDgenerate()
+path                         <- "~/PROJECTS/netreg_project/results/"
+if (!file.exists(path)) path <- "/cluster/home/simondi/results/netReg/"
+path                         <- paste0(path, "benchmark_", p, "_", uuid, ".rds")
+cat(paste("going to:", path, "\n"))
+saveRDS(m , path)
--- benchmark/run_edgenet_benchmark.sh
+++ benchmark/run_edgenet_benchmark.sh
@@ -9,7 +9,9 @@ fi
 dir=$(dirname $exe)
 scrpt="${dir}/edgenet_benchmark.R"
 
-for i in 10 20 50 100 1000 10000
-do
-  bsub -o ~/bewi/members/simondi/bsub/netReg.txt -e ~/bewi/members/simondi/bsub/ -W 120 -M 20000 -n 1 -R "rusage[mem=20000]" Rscript  $scrpt $i
-done
+bsub -o ~/bewi/members/simondi/bsub/netReg.txt -e ~/bewi/members/simondi/bsub/ -W 120 -M 20000 -n 1 -R "rusage[mem=20000]" Rscript  $scrpt -n 10 -p 10
+bsub -o ~/bewi/members/simondi/bsub/netReg.txt -e ~/bewi/members/simondi/bsub/ -W 120 -M 20000 -n 1 -R "rusage[mem=20000]" Rscript  $scrpt -n 100 -p 100
+bsub -o ~/bewi/members/simondi/bsub/netReg.txt -e ~/bewi/members/simondi/bsub/ -W 120 -M 20000 -n 1 -R "rusage[mem=20000]" Rscript  $scrpt -n 1000 -p 100
+bsub -o ~/bewi/members/simondi/bsub/netReg.txt -e ~/bewi/members/simondi/bsub/ -W 220 -M 20000 -n 1 -R "rusage[mem=20000]" Rscript  $scrpt -n 1000 -p 1000
+bsub -o ~/bewi/members/simondi/bsub/netReg.txt -e ~/bewi/members/simondi/bsub/ -W 220 -M 20000 -n 1 -R "rusage[mem=20000]" Rscript  $scrpt -n 10000 -p 1000
+bsub -o ~/bewi/members/simondi/bsub/netReg.txt -e ~/bewi/members/simondi/bsub/ -W 320 -M 20000 -n 1 -R "rusage[mem=20000]" Rscript  $scrpt -n 10000 -p 10000
