b66472401240e4f091157467618e432030890011
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-03-14 09:12:48 +0100

    [fix] added configure.win and some tests why testthat does not work on bioc when it should

--- tests/testthat/test_util.R
+++ tests/testthat/test_util.R
@@ -8,18 +8,18 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # netReg is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with netReg. If not, see <http://www.gnu.org/licenses/>.
 
 
 context("netReg utility")
- 
+
 Y <- matrix(1 + 1 : 10, 10, 1)
 X <- matrix(1 : 10, 10, 1)
 B <- matrix(1, 1, 1)
