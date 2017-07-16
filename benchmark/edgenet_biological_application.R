e524e8b2968609ace22e1c5f112ffc1fb16cfd90
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-07-16 20:31:31 +0200

    Adds data set

--- benchmark/edgenet_biological_application.R
+++ benchmark/edgenet_biological_application.R
@@ -1,1 +1,29 @@
-dat <- read.delim("~/Downloads/GSE1990_series_matrix.txt", header=T)
+library(data.table)
+library(igraph)
+library(magrittr)
+library(netReg)
+
+load("data/yeast.rda")
+X <- yeast$X[,sample(1:ncol(X), 500)]
+Y <- yeast$Y
+GY <- yeast$GY
+
+response.genes <- colnames(Y)
+graph.genes    <- rownames(GY)
+genes.intersec <- intersect(response.genes, graph.genes)
+
+Y  <-  Y[, response.genes %in% genes.intersec]
+GY <- GY[graph.genes %in% genes.intersec,
+         graph.genes %in% genes.intersec]
+
+GY.names <- rownames(GY)
+Y <- Y[, order(colnames(Y))]
+Y[1:5, 1:5]
+GY <- GY[order(rownames(GY)), order(colnames(GY))]
+
+assertthat::assert_that(all(colnames(Y) == colnames(GY)),
+                        all(colnames(Y) == rownames(GY)),
+                        all(rownames(GY) == colnames(GY)))
+
+s <- edgenet(X=X, Y=Y, G.Y=GY, lambda=10, psigx=0, psigy=1, maxit=10000, thresh=10e-5)
+
