678d8494a00b4552cb47c5cabfb36d698fb6f96e
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-10-21 22:13:07 +0200

    Minor modifications and updates to documentation

---
+++ R/data.R
@@ -0,0 +1,38 @@
+# netReg: graph-regularized linear regression models.
+#
+# Copyright (C) 2015 - 2016 Simon Dirmeier
+#
+# This file is part of netReg.
+#
+# netReg is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# netReg is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with netReg. If not, see <http://www.gnu.org/licenses/>.
+
+
+#' A sample yeast data set for regression
+#'
+#' The yeast data set is a \code{list} containing three matrices that can be used as
+#' an example for using \code{netReg}.
+#'
+#' \itemize{
+#'     \item \code{X}  (112 x 500)-dimensional binary matrix of 500 genetic markers for 112 yeast samples
+#'     \item \code{Y}  (112 x 231)-dimensional double matrix of 231 gene expression values for 112 yeast samples
+#'     \item \code{GY} (231 x 231)-dimensional adjaceny matrix representing protein-protein interactions for 231 yeast genes
+#'  }
+#'
+#' @name yeast
+#'
+#' @docType data
+#' @keywords datasets data
+#' @usage data(yeast)
+#' @format A \code{list} containing three matrices
+NA
--- R/netReg-package.R
+++ R/netReg-package.R
@@ -8,46 +8,46 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # netReg is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with netReg. If not, see <http://www.gnu.org/licenses/>.
 
 
 #' netReg
 #'
-#' \emph{netReg} is a package for generalized linear regression that includes 
+#' \emph{netReg} is a package for generalized linear regression that includes
 #' prior graphs in the models objective function.
-#' 
-#' \emph{netReg} uses \emph{Armadillo}, \emph{OpenBLAS}, 
+#'
+#' \emph{netReg} uses \emph{Armadillo}, \emph{OpenBLAS},
 #' \emph{BLAS} and \emph{LAPACK} for fast matrix computations and
 #' \emph{Dlib} for constrained derivate-free optimization.
-#' 
+#'
 #' @name netReg-package
-#' @author Simon Dirmeier | \email{mail@@simon-dirmeier.net}
+#' @author Simon Dirmeier | \email{simon.dirmeier@@gmx.de}
 #' @docType package
 #' @keywords package
-#' 
+#'
 #' @useDynLib netReg, .registration = TRUE
-#' 
-#' @references 
-#'  Friedman J., Hastie T., Hoefling H. and Tibshirani R. (2007), 
+#'
+#' @references
+#'  Friedman J., Hastie T., Hoefling H. and Tibshirani R. (2007),
 #'  Pathwise coordinate optimization.\cr
 #'  \emph{The Annals of Applied Statistics}\cr \cr
 #'  Friedman J., Hastie T. and Tibshirani R. (2010),
-#'  Regularization Paths for Generalized Linear Models 
+#'  Regularization Paths for Generalized Linear Models
 #'   via Coordinate Descent. \cr
 #'  \emph{Journal of Statistical Software}\cr \cr
 #'  Fu W. J. (1998),  Penalized Regression: The Bridge Versus the Lasso.\cr
 #'  \emph{Journal of Computational and Graphical Statistics}\cr \cr
-#'  Cheng W. and Wang W. (2014), Graph-regularized dual Lasso 
+#'  Cheng W. and Wang W. (2014), Graph-regularized dual Lasso
 #'   for robust eQTL mapping.\cr
 #'  \emph{Bioinformatics}\cr \cr
-#'  Powell M.J.D. (2009), 
+#'  Powell M.J.D. (2009),
 #'  The BOBYQA algorithm for bound constrained optimization
 #'   without derivatives.\cr
 #'  \url{http://www.damtp.cam.ac.uk/user/na/NA_papers/NA2009_06.pdf}
