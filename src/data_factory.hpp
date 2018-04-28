064ed19927ce3487afd19c0b29e1840c4e187c9b
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-04-28 02:08:48 +0200

    Updates pkgdown

--- src/data_factory.hpp
+++ src/data_factory.hpp
@@ -43,8 +43,8 @@ namespace netreg
     {
     public:
         static graph_model_data build_data(
-          double* x, double* y, double* gx, double* gy,
-          int n, int p, int q, std::string& fam)
+                double* x, double* y, double* gx, double* gy,
+                int n, int p, int q, std::string& fam)
         {
             arma::Mat<double> xm(x, n, p, false, true);
             arma::Mat<double> ym(y, n, q, false, true);
@@ -52,15 +52,15 @@ namespace netreg
             arma::Mat<double> gym(gy, q, q, false, true);
 
             netreg::graph_model_data data(
-              xm, ym, gxm, gym,
-              get_family(fam));
+                    xm, ym, gxm, gym,
+                    get_family(fam));
 
             return data;
         }
 
         static graph_model_data build_data(
-          double* x, double* y, double* gx, double* gy,
-          int* xdim, int* ydim, std::string& fam)
+                double* x, double* y, double* gx, double* gy,
+                int* xdim, int* ydim, std::string& fam)
         {
             const int n = xdim[0];
             const int p = xdim[1];
@@ -72,8 +72,15 @@ namespace netreg
             arma::Mat<double> gym(gy, q, q, false, true);
 
             netreg::graph_model_data data(
-              xm, ym, gxm, gym,
-              get_family(fam));
+                    xm, ym, gxm, gym,
+                    get_family(fam));
+
+            return data;
+        }
+
+        static graph_model_cv_data build_cv_data(
+                double* x, double* y, double* gx, double* gy,
+                int n, int p, int q, std::string& fam, int nfolds)
         {
             arma::Mat<double> xm(x, n, p, false, true);
             arma::Mat<double> ym(y, n, q, false, true);
@@ -81,16 +88,16 @@ namespace netreg
             arma::Mat<double> gym(gy, q, q, false, true);
 
             graph_model_cv_data data(
-              xm, ym, gxm, gym, nfolds,
-              get_family(fam));
+                    xm, ym, gxm, gym, nfolds,
+                    get_family(fam));
 
             return data;
         }
 
         static graph_model_cv_data build_cv_data(
-          double* x, double* y, double* gx, double* gy,
-          int* xdim, int* ydim, std::string& fam,
-          int nfolds, int lenfoldid, int* foldids)
+                double* x, double* y, double* gx, double* gy,
+                int* xdim, int* ydim, std::string& fam,
+                int nfolds, int lenfoldid, int* foldids)
         {
             const int n = xdim[0];
             const int p = xdim[1];
@@ -104,15 +111,15 @@ namespace netreg
             if (lenfoldid == xdim[0])
             {
                 graph_model_cv_data data(
-                  xm, ym, gxm, gym,
-                  nfolds, foldids, get_family(fam));
+                        xm, ym, gxm, gym,
+                        nfolds, foldids, get_family(fam));
 
                 return data;
             }
             else
             {
                 graph_model_cv_data data(
-                  xm, ym, gxm, gym, nfolds, get_family(fam));
+                        xm, ym, gxm, gym, nfolds, get_family(fam));
 
                 return data;
             }
@@ -126,17 +133,17 @@ namespace netreg
         static enum family get_family(std::string& fam)
         {
             enum family f =
-              fam == GAUSSIAN ? family::GAUSSIAN :
-              fam == BINOMIAL ? family::BINOMIAL :
-              family::NONE;
+                fam == GAUSSIAN ? family::GAUSSIAN :
+            fam == BINOMIAL ? family::BINOMIAL :
+            family::NONE;
 
             if (f == family::NONE)
             {
-                #ifdef USE_RCPPARMADILLO
+    #ifdef USE_RCPPARMADILLO
                 Rcpp::stop(FAMILY_ERROR + "\n");
-                #else
+    #else
                 throw std::invalid_argument(FAMILY_ERROR + "\n");
-                #endif
+    #endif
             }
 
             return f;
@@ -147,10 +154,3 @@ namespace netreg
 
 
 #endif //NETREG_DATA_FACTORY_HPP
-
-const std::string netreg::data_factory::GAUSSIAN = "gaussian";
-const std::string netreg::data_factory::BINOMIAL = "binomial";
-const std::string netreg::data_factory::FAMILY_ERROR =
-  "Wrong family given. Choose one of " +
-  netreg::data_factory::GAUSSIAN + "/" +
-  netreg::data_factory::BINOMIAL;
